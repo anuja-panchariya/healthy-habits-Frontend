8c4cad2cfaa5bea1b007a487c50ab387cb53b4d6
Anuja Panchariya <anujapanchariya7@gmail.com>
2026-03-02 15:21:27 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import habitsReducer from './habitsSlice'
+import { habitsSlice } from './habitsSlice'
+import { useAppDispatch, useAppSelector } from '../hooks'
 
 export const store = configureStore({
   reducer: {
-    habits: habitsReducer,
+    habits: habitsSlice.reducer  
   },
 })
+
+// Export types
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
