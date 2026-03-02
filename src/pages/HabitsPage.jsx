bd782fed60b681678e320d94da8c91529a2270b9
Anuja Panchariya <anujapanchariya7@gmail.com>
2026-03-02 23:15:22 +0530

    Update HabitsPage.jsx

--- src/pages/HabitsPage.jsx
+++ src/pages/HabitsPage.jsx
@@ -1,4 +1,4 @@
-\
+
 import React, { useEffect, useState, useCallback } from 'react'
 import WellnessScoreCard from '../components/WellnessScoreCard'  
 import { getHabits, createHabit, deleteHabit } from '../lib/api'
