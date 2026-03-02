b7228fa5a6f7a3d83cb71a44f4a7e2524c3371bf
Anuja Panchariya <anujapanchariya7@gmail.com>
2026-03-02 21:39:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'           
 import { store } from './store/store'            
+import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.js'
 import './index.css'
 
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>                    
-      <App />
-    </Provider>                                
+    <ClerkProvider 
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      afterSignInUrl="/dashboard"    
+      afterSignUpUrl="/dashboard"   
+    >
+      <Provider store={store}>                    
+        <App />
+      </Provider>                                  
+    </ClerkProvider>
   </React.StrictMode>,
 )
