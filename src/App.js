59f2961e3723b263fcb5c786dacc02c65edede6d
Anuja Panchariya <anujapanchariya7@gmail.com>
2026-03-02 22:15:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn, useAuth } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, RedirectToSignIn, useAuth } from '@clerk/clerk-react'
 import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
@@ -12,17 +12,6 @@ import HabitsPage from './pages/HabitsPage'
 import AnalyticsPage from './pages/AnalyticsPage'
 import ChallengesPage from './pages/ChallengesPage'
 import ProfilePage from './pages/ProfilePage'
-
-const CLERK_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
-
-function ClerkProviderWithNavigate({ children }) {
-  const navigate = useNavigate()
-  return (
-    <ClerkProvider publishableKey={CLERK_KEY} navigate={(to) => navigate(to)}>
-      {children}
-    </ClerkProvider>
-  )
-}
 
 function ProtectedRoute({ children }) {
   const { isSignedIn, isLoaded } = useAuth()
@@ -43,62 +32,60 @@ function App() {
     <ThemeProvider defaultTheme="light" storageKey="healthyhabits-theme">
       <Provider store={store}>
         <BrowserRouter>
-          <ClerkProviderWithNavigate>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route
-                path="/dashboard"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <Dashboard />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/habits"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <HabitsPage />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/analytics"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <AnalyticsPage />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/challenges"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <ChallengesPage />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/profile"
-                element={
-                  <ProtectedRoute>
-                    <Layout>
-                      <ProfilePage />
-                    </Layout>
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
-            <Toaster richColors position="top-right" />
-          </ClerkProviderWithNavigate>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <Dashboard />
+                  </Layout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/habits"
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <HabitsPage />
+                  </Layout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/analytics"
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <AnalyticsPage />
+                  </Layout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/challenges"
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <ChallengesPage />
+                  </Layout>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <ProfilePage />
+                  </Layout>
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <Toaster richColors position="top-right" />
         </BrowserRouter>
       </Provider>
     </ThemeProvider>
