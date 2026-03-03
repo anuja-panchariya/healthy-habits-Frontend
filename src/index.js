701393134b58dfdc6a79c66a8577d771855da8b5
Anuja Panchariya <anujapanchariya7@gmail.com>
2026-03-03 12:12:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-// YE FINAL - ONLY EK ClerkProvider!
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'           
-import { store } from './store/store'            
-import { ClerkProvider } from '@clerk/clerk-react'
-import App from './App.js'
 import './index.css'
+import App from './App'
 
-const CLERK_PUBLISHABLE_KEY = "pk_test_cG93ZXJmdWwtcmF0dGxlci0zNi5jbGVyay5hY2NvdW50cy5kZXYk"
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <ClerkProvider 
-      publishableKey={CLERK_PUBLISHABLE_KEY}
-      fallbackRedirectUrl="/dashboard"  // Updated prop name
-    >
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ClerkProvider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
