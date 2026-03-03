615447353608211a134db224902420518f692b76
Anuja Panchariya <anujapanchariya7@gmail.com>
2026-03-03 14:08:44 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,18 +1,19 @@
-import axios from 'axios'
+export const API_URL = 'https://healthy-habits-be-1.onrender.com/api'
 
-const API_URL = process.env.REACT_APP_BACKEND_URL
-
-export const api = axios.create({
-  baseURL: API_URL,
-  withCredentials: true,
-})
-
-export const setAuthToken = (token) => {
-  if (token) {
-    api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-  } else {
-    delete api.defaults.headers.common['Authorization']
+export const apiFetch = async (endpoint, options = {}) => {
+  const url = `${API_URL}${endpoint}`
+  const config = {
+    headers: { 
+      'Content-Type': 'application/json', 
+      ...options.headers 
+    },
+    ...options
   }
+  
+  const response = await fetch(url, config)
+  if (!response.ok) {
+    const errorText = await response.text()
+    throw new Error(`API ${response.status}: ${errorText}`)
+  }
+  return response.json()
 }
-
-export default api
